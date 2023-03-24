7422797513d6d80b26d6a4df74fc340d32f4a3de
Ahmed Mubarak <ahmed.elia2003@gmail.com>
2023-03-24 22:31:14 +0200

    inital commit - task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+file { '/tmp/school':
+    path    => '/tmp/school',
+    ensure  => present,
+    content => 'I love Puppet',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744'
+}
