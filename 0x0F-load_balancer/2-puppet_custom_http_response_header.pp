f1752e8c926f34e97f953ec631522e0f7dfadb3f
Ahmed Mubarak <ahmed.elia2003@gmail.com>
2023-05-05 21:20:35 +0300

    https -- haproxy

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,6 +3,6 @@ exec { 'custom name':
   command  => 'sudo apt update -y;
 	sudo apt install nginx -y;
 	sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
-	sudo service nginx sestart',
+	sudo service nginx restart',
   provider => shell
 }
