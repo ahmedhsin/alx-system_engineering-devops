69eb70d4aace9c17a72ff693303e19e1e09b91ff
Ahmed Mubarak <ahmedmubarak.hsin@gmail.com>
2023-08-09 14:21:24 +0200

    testing task 2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""scrip for gather data from an api"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], next=''):
+    """ret num of sub to given reddit"""
+    print(next)
+
+    userAgent = """Mozilla/5.0 (X11; Linux x86_64)\
+     AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"""
+    url = 'https://www.reddit.com/r/{}/top.json?' + next
+    r = requests.get(url.format(subreddit), headers={'user-agent': userAgent})
+    try:
+        children = r.json()['data']['children']
+        hot_list = hot_list + children
+        after = r.json()['data']['after']
+        next = f'after={after}'
+        if (next.split('=')[1] == 'None'):
+            return hot_list
+        return recurse(subreddit, hot_list, next)
+    except KeyError:
+        return None
